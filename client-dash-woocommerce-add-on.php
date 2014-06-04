c9eb022cc5dadea3b4d35e01923fae20c2fc105b
Kyle <kyle@realbigmarketing.com>
2014-06-03 21:14:28 -0400

    First commit
    Placeholder

---
+++ client-dash-woocommerce-add-on.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Client Dash Woocommerce Add-on
+Description: Integrates content from Woocommerce with Client Dash by displaying it on the reports page.
+Version: 0.1
+Author: Kyle Maurer
+Author URI: http://realbigmarketing.com/staff/kyle
+*/
